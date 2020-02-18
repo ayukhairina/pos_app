0ef7f4fc2f571ce21deca748499a055c72dcf06f
ayukhairina <wahyukhairina@gmail.com>
2020-02-18 08:06:32 +0800

    add standard response

--- src/models/product.js
+++ src/models/product.js
@@ -1,58 +1,57 @@
 const connection = require('../config/mysql')
 
 module.exports = {
-    getAll : (searchName, sortBy) => {
-        return new Promise ((resolve,reject) => {
-            
-                connection.query(`SELECT * FROM product WHERE product_name LIKE '%${searchName}%'`, (error, result) => {
-                if (error) reject(new Error(error))
-                resolve(result)   
-                })
-            })
-     },
+  getAll: (searchName, sortBy) => {
+    return new Promise((resolve, reject) => {
+      connection.query(`SELECT * FROM product WHERE product_name LIKE '%${searchName}%'`, (error, result) => {
+        if (error) reject(new Error(error))
+        resolve(result)
+      })
+    })
+  },
 
-     sortData : (column) =>{
-         return new Promise ((resolve,reject) => {
-             connection.query(`SELECT * FROM product ORDER BY ${column} ASC`,  (error,result) => {
-             if (error) reject(new Error(error))
-             resolve(result)
-         })
-     })
- },
+  sortData: (column) => {
+    return new Promise((resolve, reject) => {
+      connection.query(`SELECT * FROM product ORDER BY ${column} ASC`, (error, result) => {
+        if (error) reject(new Error(error))
+        resolve(result)
+      })
+    })
+  },
 
-    getDetail: (productId) => {
-        return new Promise((resolve, reject) => {
-            connection.query('SELECT * FROM product WHERE product_code = ?', productId, (error, result) => 
-            { if (error) reject(new Error(error))
-                resolve(result)
-            })
-        })
-    },
+  getDetail: (productId) => {
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM product WHERE product_code = ?', productId, (error, result) => {
+        if (error) reject(new Error(error))
+        resolve(result)
+      })
+    })
+  },
 
-    insertData: (data) => {
-        return new Promise ((resolve, reject) => {
-            connection.query('INSERT INTO product SET ?', data, (error, result) => {
-                if (error) reject(new Error(error))
-                resolve(result)
-            })
-        })
-    },
+  insertData: (data) => {
+    return new Promise((resolve, reject) => {
+      connection.query('INSERT INTO product SET ?', data, (error, result) => {
+        if (error) reject(new Error(error))
+        resolve(result)
+      })
+    })
+  },
 
-    updateData: (data, productId) => {
-        return new Promise((resolve, reject) => {
-            connection.query('UPDATE product SET ? WHERE product_code = ?', [data, productId], (error, result) => {
-                if (error) reject(new Error(error))
-                resolve(result)
-            } )
-        })
-    },
+  updateData: (data, productId) => {
+    return new Promise((resolve, reject) => {
+      connection.query('UPDATE product SET ? WHERE product_code = ?', [data, productId], (error, result) => {
+        if (error) reject(new Error(error))
+        resolve(result)
+      })
+    })
+  },
 
-    deleteData: (productId) => {
-        return new Promise((resolve,reject) => {
-            connection.query('DELETE FROM product WHERE product_code = ?', productId, (error,result) => {
-                if (error) reject(new Error(error))
-                resolve(result)
-            })
-        })
-    }
+  deleteData: (productId) => {
+    return new Promise((resolve, reject) => {
+      connection.query('DELETE FROM product WHERE product_code = ?', productId, (error, result) => {
+        if (error) reject(new Error(error))
+        resolve(result)
+      })
+    })
+  }
 }
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const Route = express.Router()
 
-const {getAll,sortData, getDetail, insertData, updateData, deleteData } = require('../controllers/product')
+const { getAll, sortData, getDetail, insertData, updateData, deleteData } = require('../controllers/product')
 
 Route
-    .get('/', getAll)
-     .get('/sort', sortData)
-    .get('/:productId', getDetail)
-    .post('/', insertData)
-    .patch('/:productId', updateData)
-    .delete('/:productId', deleteData)
+  .get('/', getAll)
+  .get('/sort', sortData)
+  .get('/:productId', getDetail)
+  .post('/', insertData)
+  .patch('/:productId', updateData)
+  .delete('/:productId', deleteData)
 
-    module.exports = Route
+module.exports = Route
